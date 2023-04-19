1d38ef586c27b0e782b0c6e9e2f03f8680cbe84d
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-04-19 14:30:06 +0530

    configure index and env

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import BlogRoutes from "./routes/BlogRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const PARAMS = {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+};
+const URI = process.env.MONGOOSE_URI;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/blogs", BlogRoutes);
+
+mongoose.set("strictQuery", false);
+mongoose.connect(URI, PARAMS)
+    .then(() => app.listen(PORT, 
+        () => console.info(`Server running on PORT ${PORT} 🔥`)))
+    .catch((err) => console.error(err.message));
