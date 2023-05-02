430cce21e4c8fbdcaae6268f03b641084d7a2c46
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-05-02 06:54:34 +0530

    create hooks and authentication pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 import Header from './components/TouristHome/Header'
 import Footer from './components/TouristHome/Footer'
 import LandingPage from './components/TouristHome/LandingPage'
 import ViewGuides from './components/TouristHome/ViewGuides'
+import { useAuthContext } from './hooks/useAuthContext';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import ForgetPassword from "./pages/ForgetPassowrd";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
+  const { user } = useAuthContext()
 
     return (
         <React.Fragment>
@@ -18,6 +24,10 @@ function App() {
                 <Routes>
                     <Route path="/" element={<LandingPage />} exact></Route>
                     <Route path="/guides" element={<ViewGuides />} exact></Route>
+                    <Route path="/signup" element={!user ? <Signup /> :<Navigate to="/login"></Navigate>}/>
+                    <Route path="/login" element={!user ? <Login /> :<Navigate to="/"></Navigate>}/>
+                    <Route path="/reset-password" element={!user ? <ForgetPassword /> :<Navigate to="/"></Navigate>}/>
+                    <Route path="/api/user/reset-password/:token" element={<ResetPassword />} exact></Route>
                 </Routes>
             </main>
 
