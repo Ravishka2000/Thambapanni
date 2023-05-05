24b0ee6f6c7022ae23db30671437801660683a18
Pravini Wick <85413981+Pravini15@users.noreply.github.com>
2023-05-05 23:06:08 +0530

    View bookings

--- client/src/components/Guide/Bookings.jsx
+++ client/src/components/Guide/Bookings.jsx
@@ -8,71 +8,86 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useAuthContext } from "../../hooks/useAuthContext"
 
 const Bookings = () => {
 
-    const [booking, setbooking] = useState();
+    const [bookings, setbookings] = useState();
+    const { user } = useAuthContext()
 
     useEffect(() => {
         fetch("http://localhost:7070/api/booking/bookings")
             .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                setbooking(data);
-                console.log(booking);
+            .then((data) => {              
+                setbookings(data);              
             })
             .catch((err) => console.log(err));
     }, []);
 
     useEffect(() => {
-        if (booking) {
-            console.log(booking);
+        if (bookings) {
+            console.log(bookings);
         }
-    }, [booking]);
+    }, [bookings]);
 
     return (
 
       <div>
-        <Box height={20}/>
+        <Box height={80}/>
       <Box sx={{ overflowX: "hidden", marginTop: "20px" }}
           display="flex"
           justifyContent="center"
           alignItems="center"
           flexDirection={"column"}>
-          <Typography sx={{ width: 800, margin: "auto", padding: "20px" }}
-              variant="h4">
+          <Typography variant="h5" style={{ marginBottom: '16px', fontWeight: 'bold', color: '#19376D', padding: '3', }}>
               My Bookings
           </Typography>
-          <TableContainer component={Paper} sx={{ margin: "auto", width: 650 }} elevation={0}>
+          <TableContainer component={Paper} sx={{ margin: "auto", width: 1200 }} elevation={0}>
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
                   <TableHead>
                       <TableRow>
-                          <TableCell>Order Id</TableCell>
-                          <TableCell align="right">Amount</TableCell>
+                          <TableCell>Booking Id</TableCell>
+                          <TableCell align="right">Name</TableCell>
                           <TableCell align="right">Date</TableCell>
+                          <TableCell align="right">Location</TableCell>
+                          <TableCell align="right">Group size</TableCell>
+                          <TableCell align="right">Phone</TableCell>
+                          <TableCell align="right">Email</TableCell>
                           <TableCell align="right">Status</TableCell>
                       </TableRow>
                   </TableHead>
                   <TableBody>
-                      {booking && booking.map((booking) => (
-                          <TableRow
-                              key={booking._id}
-                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                          >
-                              <TableCell component="th" >
-                                  {booking._id.slice(0, 10)}
-                              </TableCell>
-                              <TableCell component="th" align="right">
-                                  {booking.paymentIntent.amount}
-                              </TableCell>
-                              <TableCell component="th" align="right" >
-                                  {new Date(booking.createdAt).toLocaleDateString()}
-                              </TableCell>
-                              <TableCell component="th" align="right">
-                                  {booking.Status}
-                              </TableCell>
-                          </TableRow>
-                      ))}
+                  {bookings && bookings.filter(booking => booking.Customer === user._id).map(booking => (
+                      <TableRow
+                        key={booking._id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell component="th" >
+                    {booking._id}
+                    </TableCell>
+                    <TableCell component="th" align="right">
+                    {booking.name}
+                    </TableCell>
+                    <TableCell component="th" align="right" >
+                    {new Date(booking.tourDate).toLocaleDateString()}
+                    </TableCell>
+                    <TableCell component="th" align="right" >
+                    {booking.tourLocation}
+                    </TableCell>
+                    <TableCell component="th" align="right" >
+                    {booking.groupSize}
+                    </TableCell>
+                    <TableCell component="th" align="right" >
+                    {booking.phone}
+                    </TableCell>
+                    <TableCell component="th" align="right" >
+                    {booking.email}
+                    </TableCell>
+                    <TableCell component="th" align="right" >
+                    {booking.Status}
+                    </TableCell>
+                      </TableRow>
+                     ))}
+                      
 
                   </TableBody>
               </Table>
