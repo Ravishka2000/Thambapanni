9c47fe5b2ddb3842d1f2705329757b6dee54e66e
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-05-16 12:20:44 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -21,7 +21,12 @@ const URI = process.env.MONGOOSE_URI;
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        "https://thambapanni-app.onrender.com"
+    ]
+}));
 
 app.use("/api/blogs", BlogRoutes);
 app.use("/api/auth", UserRoutes);
